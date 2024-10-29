200ceae5f43b322ace004c7d5add26153e7b637a
Kumaraguru Muthuraj <kumaraguru1976@yahoo.com>
2024-10-29 20:33:51 +0530

    Bubble sort

--- src/main/java/com/bhriguai/sorting/BubbleSort.java
+++ src/main/java/com/bhriguai/sorting/BubbleSort.java
@@ -1,4 +1,31 @@
 package com.bhriguai.sorting;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class BubbleSort {
+    static ArrayList<Integer> bubble_sort(ArrayList<Integer> arr) {
+        int len = arr.size();
+        for (int i = 0; i < len-1; i++) {
+            for (int s = len - 1; s > i; s--) {
+                if (arr.get(s) < arr.get(s - 1)) {
+                    swap(arr, s, s - 1);
+                }
+            }
+        }
+        return arr;
+    }
+
+    static void swap(ArrayList<Integer> l, int i, int j) {
+        Integer temp = l.get(i);
+        l.set(i, l.get(j));
+        l.set(j, temp);
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> nums = new ArrayList<>(Arrays.asList(34, 10, 45, 23, 12, 9, 6, 7, 2));
+        bubble_sort(nums);
+
+    }
+
 }
