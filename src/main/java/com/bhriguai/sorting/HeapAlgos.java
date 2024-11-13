825a4c032fdf054469f454761b5cae436936807f
Kumaraguru Muthuraj <kumaraguru1976@yahoo.com>
2024-11-13 21:14:34 +0530

    Merge sort completed

--- src/main/java/com/bhriguai/sorting/HeapAlgos.java
+++ src/main/java/com/bhriguai/sorting/HeapAlgos.java
@@ -2,16 +2,18 @@ package com.bhriguai.sorting;
 
 public class HeapAlgos {
     //NOTE - Storing the heap begins from the index 1. Index 0 is not used, set to MIN ELEMENT.
+
     /* To build a max-heap in-place, traverse from the right most element, when an element with a child is found, heapify down
-    from that element.
+    * from that element.
     */
+
     /* For in-place sorting of a max-heap, swap the root with the last element. So max goes to the last. Consider the last element is removed.
     * The heap size reduces by 1. Heapify down.
     * Repeat the same till the heap's virtual size reduces to 1.
-    *
-     */
-    /*
-    Sorting the heap in-place.
-     */
+    */
 
+    /*
+    * Sorting the max-heap in-place. Swap the root with the last element. So the last element found its place.
+    * Reduce the heap size virtually by 1. Heapify from the root.
+    */
 }
---
+++ src/main/java/com/bhriguai/sorting/MergeSort.java
@@ -0,0 +1,67 @@
+package com.bhriguai.sorting;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MergeSort {
+
+
+    static void helper(ArrayList<Integer> arr, int l, int h) {
+        if (l < h) {
+            int m = l + (h - l) / 2;
+            helper(arr, l, m);
+            helper(arr, m + 1, h);
+
+            ArrayList<Integer> aux = new ArrayList<>();
+            int i = l;
+            int j = m+1;
+            while (i <= m && j <= h) {
+                if (arr.get(i) < arr.get(j)) {
+                    aux.add(arr.get(i));
+                    i++;
+                } else {
+                    aux.add(arr.get(j));
+                    j++;
+                }
+            }
+            // BUG - Earlier I thought that only 1 element is left and there was a bug.
+            // You have to copy all of the left out elements in either of the partition.
+            for (; i <= m; i++) {
+                aux.add(arr.get(i));
+            }
+            for (; j <= h; j++) {
+                aux.add(arr.get(j));
+            }
+            //Copy back
+            for (int k = 0; k < aux.size(); k++, l++) {
+                arr.set(l, aux.get(k));
+            }
+        }
+    }
+
+    static ArrayList<Integer> merge_sort(ArrayList<Integer> arr) {
+        // Write your code here.
+        helper(arr, 0, arr.size() - 1);
+        return arr;
+    }
+
+    static void swap(ArrayList<Integer> l, int i, int j) {
+        Integer temp = l.get(i);
+        l.set(i, l.get(j));
+        l.set(j, temp);
+    }
+
+    static void print(ArrayList<Integer> arr) {
+        for (Integer i : arr) {
+            System.out.print(i + ", ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> nums = new ArrayList<>(Arrays.asList(10, 7, 5, 3, 8, 6, 4, 2));
+        merge_sort(nums);
+        print(nums);
+    }
+
+}
