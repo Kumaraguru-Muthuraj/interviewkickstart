0cbe37b164dbe8b90216944e1f2c0d7150e5ca87
Kumaraguru Muthuraj <kumaraguru1976@yahoo.com>
2024-11-16 17:59:24 +0530

    HeapSort with building a heap inplace

--- src/main/java/com/bhriguai/sorting/HeapSort.java
+++ src/main/java/com/bhriguai/sorting/HeapSort.java
@@ -3,27 +3,33 @@ package com.bhriguai.sorting;
 import java.util.*;
 
 public class HeapSort {
+    /*
+    The heapify operation should be called from n/2 index downwards, because the items from n/2 + 1 don't have children.
+    Heapify should be iterative.
+    Infact any recursive should be done iteratively.
+     */
 
-    int getMaxIdxOfChild(ArrayList<Integer> h, int i) {
+    int getMaxIdxOfChild(ArrayList<Integer> h, int i, int size) {
         int lI = i * 2;
         int rI = lI + 1;
-        if (lI >= h.size()) {
+        if (lI >= size) {
             return Integer.MIN_VALUE;
         }
-        if (rI < h.size() && h.get(rI) > h.get(lI)) {
+        if (rI < size && h.get(rI) > h.get(lI)) {
             return rI;
         }
         return lI;
     }
-    void heapifyDown(ArrayList<Integer> h, int i) {
-        int mI = getMaxIdxOfChild(h, i);
+
+    void heapifyDownWithVirtualMax(ArrayList<Integer> h, int i, int size) {
+        int mI = getMaxIdxOfChild(h, i, size);
         if (mI <= 0) {
             return;
         }
         // BE SURE THAT THE HEAPIFY IS CALLED ON THE CHILD
         if (h.get(mI) > h.get(i)) {
             Collections.swap(h, mI, i);
-            heapifyDown(h, mI);
+            heapifyDownWithVirtualMax(h, mI, size);
         }
     }
 
@@ -31,22 +37,30 @@ public class HeapSort {
         //NOTE - Storing the heap begins from the index 1. Index 0 is not used, set to MIN ELEMENT.
         list.add(0, Integer.MIN_VALUE);
         for (int i = list.size() - 1; i > 0; i--) {
-            heapifyDown(list, i);
+            heapifyDownWithVirtualMax(list, i, list.size());
         }
         return list;
     }
-    void sort() {
 
+    void heap_sort(ArrayList<Integer> h) {
+        buildHeap(h);
+
+        int size = h.size();
+        for (int idx = size - 1; idx > 1; idx--) {
+            Collections.swap(h, 1, idx);
+            heapifyDownWithVirtualMax(h, 1, idx);
+        }
+        h.remove(0);
     }
 
     public static void main(String[] args) {
         //Utils.getNumbers(10);
-        ArrayList<Integer> l = new ArrayList<>(Arrays.asList(-5, 8, 9, 15, 11, 14, -13, 17, 20, 30));
+        ArrayList<Integer> l = Utils.getNumbers(20);//new ArrayList<>(Arrays.asList(-5, 8, 9, 15, 11, 14, -13, 17, 20, 30));
         Utils.print(l);
 
         HeapSort h = new HeapSort();
-        h.buildHeap(l);
-        h.sort();
+        //h.buildHeap(l);
+        h.heap_sort(l);
         Utils.print(l);
 
         System.out.println("************");
