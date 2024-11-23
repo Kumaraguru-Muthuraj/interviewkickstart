6611a2b06f429457c1e41958894c5c097d6ca125
Kumaraguru Muthuraj <kumaraguru1976@yahoo.com>
2024-11-23 08:17:05 +0530

    Different version to submit in IK

--- src/main/java/com/bhriguai/recursion/foundation/GenerateAllSubsets0.java
+++ src/main/java/com/bhriguai/recursion/foundation/GenerateAllSubsets0.java
@@ -18,10 +18,34 @@ public class GenerateAllSubsets0 {
         }
     }
 
+    static ArrayList<String> list;
+    static ArrayList<String> generateSubsets2(String slate, List<Character> lst) {
+        if (lst.size() == 0) {
+            list.add(slate);
+        } else {
+            generateSubsets2(slate, lst.subList(1, lst.size()));
+            generateSubsets2(slate + lst.get(0), lst.subList(1, lst.size()));
+        }
+        return list;
+    }
+
+    static ArrayList<String> generate_all_subsets(String s) {
+        list = new ArrayList<>();
+        ArrayList<Character> C = new ArrayList<>();
+        char[] chars = s.toCharArray();
+        for (char c : chars) {
+            C.add(c);
+        }
+        return generateSubsets2("", C);
+    }
+
     public static void main(String[] args) {
         Character a[]
-                = new Character[] { '1', '2', '3'};
-        printSubsets(Arrays.asList(a));
-
+                = new Character[] { 'x', '2', '3'};
+        //printSubsets(Arrays.asList(a));
+        ArrayList<String> l = generate_all_subsets("123");
+        for (String s : l) {
+            System.out.println(s);
+        }
     }
 }
