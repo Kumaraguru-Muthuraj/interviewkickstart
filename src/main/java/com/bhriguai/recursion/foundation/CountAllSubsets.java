33eba4f599087c11490cb919e2beb04da82aec75
Kumaraguru Muthuraj <kumaraguru1976@yahoo.com>
2024-11-24 07:57:46 +0530

    Count subsets

--- src/main/java/com/bhriguai/recursion/foundation/CountAllSubsets.java
+++ src/main/java/com/bhriguai/recursion/foundation/CountAllSubsets.java
@@ -1,4 +1,15 @@
 package com.bhriguai.recursion.foundation;
 
 public class CountAllSubsets {
+    static Integer count_all_subsets(Integer n) {
+        if (n < 1) return 1;
+        if (n == 1) {
+            return 2;
+        } else {
+            return 2 * count_all_subsets(n-1);
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println(count_all_subsets(2));
+    }
 }
