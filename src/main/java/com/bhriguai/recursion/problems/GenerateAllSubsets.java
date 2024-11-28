0bf6ba6c4dec601afc247ec8c9faf41c7fbf0427
Kumaraguru Muthuraj <kumaraguru1976@yahoo.com>
2024-11-28 20:58:42 +0530

    Recursion

---
+++ src/main/java/com/bhriguai/recursion/problems/GenerateAllSubsets.java
@@ -0,0 +1,4 @@
+package com.bhriguai.recursion.problems;
+
+public class GenerateAllSubsets {
+}
---
+++ src/main/java/com/bhriguai/recursion/problems/GenerateTargetSumCombinations.java
@@ -0,0 +1,30 @@
+package com.bhriguai.recursion.problems;
+/*
+Generate All Combinations With Sum Equal To Target
+Given an integer array, generate all the unique combinations of the array numbers that sum up to a given target value.
+
+Example One
+{
+"arr": [1, 2, 3],
+"target": 3
+}
+Output:
+
+[
+[3],
+[1, 2]
+]
+Example Two
+{
+"arr": [1, 1, 1, 1],
+"target": 2
+}
+Output:
+
+[
+[1, 1]
+]
+*/
+public class GenerateTargetSumCombinations {
+
+}
