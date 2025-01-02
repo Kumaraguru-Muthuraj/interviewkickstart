35176deac236daa6911f03afd38bb2a8e2c9a01b
Kumaraguru Muthuraj <kumaraguru1976@yahoo.com>
2025-01-02 20:46:54 +0530

    Graphs

--- src/main/java/com/bhriguai/graphs/foundation/EdgeListToAdjacencyMatrix.java
+++ src/main/java/com/bhriguai/graphs/foundation/EdgeListToAdjacencyMatrix.java
@@ -1,4 +1,30 @@
 package com.bhriguai.graphs.foundation;
 
 public class EdgeListToAdjacencyMatrix {
+    public static void main(String[] args) {
+        GraphByAdjacencyMatrix g = new GraphByAdjacencyMatrix(10);
+
+        g.addEdge(0,1);
+        g.addEdge(0,6);
+        g.addEdge(0,8);
+
+        g.addEdge(1, 4);
+        g.addEdge(1, 6);
+        g.addEdge(1, 9);
+
+        g.addEdge(2, 4);
+        g.addEdge(2, 6);
+
+        g.addEdge(3, 4);
+        g.addEdge(3, 5);
+        g.addEdge(3, 8);
+
+        g.addEdge(4, 5);
+        g.addEdge(4, 9);
+
+        g.addEdge(7, 8);
+        g.addEdge(7, 9);
+
+        g.printGraph();
+    }
 }
---
+++ src/main/java/com/bhriguai/graphs/foundation/GraphByAdjacencyMatrix.java
@@ -0,0 +1,28 @@
+package com.bhriguai.graphs.foundation;
+
+import java.util.Arrays;
+
+public class GraphByAdjacencyMatrix {
+    public GraphByAdjacencyMatrix(int V) {
+        graph = new int[V][V];
+    }
+
+    public void addEdge(int i, int j) {
+        graph[i][j] = 1;
+        graph[j][i] = 1;
+    }
+
+    public void printGraph() {
+        for (int i = 0; i < graph.length; i++) {
+            System.out.print(i + " - ");
+            for (int j = 0; j < graph[0].length; j++) {
+                if (graph[i][j] == 1) {
+                    System.out.print(j + ", ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+    int[][] graph;
+}
