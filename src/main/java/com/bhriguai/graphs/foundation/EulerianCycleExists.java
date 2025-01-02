6f1e7db587942ab09b24e0acd8687fa8b0e22318
Kumaraguru Muthuraj <kumaraguru1976@yahoo.com>
2025-01-02 19:20:16 +0530

    Graphs

--- src/main/java/com/bhriguai/graphs/foundation/EulerianCycleExists.java
+++ src/main/java/com/bhriguai/graphs/foundation/EulerianCycleExists.java
@@ -1,4 +1,8 @@
 package com.bhriguai.graphs.foundation;
 
 public class EulerianCycleExists {
+    /*
+    Check for multiple connected components.
+    If there are multiple components, there is no Eulerian cycle.
+     */
 }
