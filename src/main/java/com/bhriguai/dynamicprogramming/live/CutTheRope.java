0530b1b23622766da6803df657f0ea20af876ab3
Kumaraguru Muthuraj <kumaraguru1976@yahoo.com>
2025-01-25 09:54:20 +0530

    Timed Test

---
+++ src/main/java/com/bhriguai/dynamicprogramming/live/CutTheRope.java
@@ -0,0 +1,20 @@
+package com.bhriguai.dynamicprogramming.live;
+
+/*
+Given a rope, cut it into parts maximizing the product of their lengths.
+
+Example
+{
+"n": 4
+}
+Output:
+
+4
+Length of the rope is 4.
+All ways it can be cut into parts: 1+3, 1+1+2, 1+1+1+1, 2+2, 2+1+1.
+Among these, 2+2 cut makes for the greatest product: 2*2=4.
+
+Notes
+ */
+public class CutTheRope {
+}
---
+++ src/main/java/com/bhriguai/dynamicprogramming/live/NumberOfPathsInMatrix.java
@@ -0,0 +1,12 @@
+package com.bhriguai.dynamicprogramming.live;
+
+/*
+Number Of Paths In A Matrix
+Given a matrix n by m containing 0s and 1s, find the number of distinct paths from cell (0, 0) to cell (n - 1, m - 1) that
+
+step on cells with 1s and
+go only down or to the right.
+Example:
+ */
+public class NumberOfPathsInMatrix {
+}
